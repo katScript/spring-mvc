17b50482de5ca7aa04b1609f7f821cbe446b6c83
kat <kat@kat.com>
2022-10-19 03:42:56 +0700

    SPR-00:Quick start

---
+++ src/main/java/org/spring/App.java
@@ -0,0 +1,15 @@
+package org.spring;
+
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
